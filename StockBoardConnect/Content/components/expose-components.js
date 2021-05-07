becb527acffd8644a317bce6874043e514f6b813
mitsutaka <mitsutaka.murao@tryeting.jp>
2021-05-07 22:10:26 +0900

    キャッシュ削除

---
+++ StockBoardConnect/Content/components/expose-components.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
+
+import { Tutorial } from './Tutorial.jsx';
+
+// any css-in-js or other libraries you want to use server-side
+import { ServerStyleSheet } from 'styled-components';
+import Helmet from 'react-helmet';
+
+global.React = React;
+global.ReactDOM = ReactDOM;
+global.ReactDOMServer = ReactDOMServer;
+
+global.Styled = { ServerStyleSheet };
+global.Helmet = Helmet;
+
+global.Components = { Tutorial};
